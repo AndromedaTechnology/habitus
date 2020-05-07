ef93e5731ccddc404abec5784a8ffcd673eaee2e
Mario <marionovak5@gmail.com>
2020-05-08 01:06:47 +0200

    User Header: removed scss

--- src/components/UserHeader.vue
+++ src/components/UserHeader.vue
@@ -15,18 +15,4 @@ export default class UserHeader extends Vue {
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
