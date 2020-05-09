de815f1cf0f96eb6270c5b84ee0d02533970eb59
Mario <marionovak5@gmail.com>
2020-05-10 01:37:58 +0200

    Activity Header: showing X days ago

--- src/components/ActivityHeader.vue
+++ src/components/ActivityHeader.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="activityHeader">
-    <h1>
+    <h1 class="amount">
       <span>{{ activity.amount }}</span>
+      <button class="delete" @click="deleteActivity()">Delete</button>
     </h1>
-    <button @click="deleteActivity()">Delete</button>
+    <span>{{ ago }} days ago</span>
   </div>
 </template>
 
@@ -15,10 +16,29 @@ import { Activity } from "../store/activity/types";
 export default class ActivityHeader extends Vue {
   @Prop() private activity!: Activity;
 
+  get ago() {
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const diffSeconds =
+      new Date().getTime() - new Date(this.activity.createdAt).getTime();
+
+    if (!diffSeconds) return 0;
+
+    const diffDays = Math.round(diffSeconds / oneDay);
+    return diffDays;
+  }
+
   deleteActivity() {
     this.$emit("delete", this.activity);
   }
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.amount {
+  display: flex;
+}
+
+button.delete {
+  margin-left: 8px;
+}
+</style>
