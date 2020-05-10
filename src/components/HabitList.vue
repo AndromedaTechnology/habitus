89257eb4a740c04a79042331c3bed246ef95ef27
Mario <marionovak5@gmail.com>
2020-05-10 03:07:50 +0200

    Habit List: as a separate component

---
+++ src/components/HabitList.vue
@@ -0,0 +1,84 @@
+<template>
+  <div v-if="habits && user">
+    <div v-for="habit in habits" :key="habit.name" class="habit">
+      <HabitHeader
+        :habit="habit"
+        :activities="heatmapHabitActivities(habit._id)"
+      />
+
+      <ActivityCreate
+        @submit="activityCreateSubmit(habit._id, user._id, $event)"
+      />
+
+      <ActivityChart
+        class="activityChart"
+        v-if="heatmapHabitActivities(habit._id)"
+        :activities="heatmapHabitActivities(habit._id)"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Action, Getter } from "vuex-class";
+
+import { Habit } from "@/store/habit/types";
+import { User } from "@/store/user/types";
+import { Activity, Activities } from "@/store/activity/types";
+import HabitHeader from "@/components/HabitHeader.vue";
+import ActivityChart from "@/components/ActivityChart.vue";
+import ActivityCreate from "@/components/ActivityCreate.vue";
+
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "HabitList",
+  components: {
+    HabitHeader,
+    ActivityChart,
+    ActivityCreate,
+  },
+})
+export default class HabitList extends Vue {
+  @Prop() private habits!: Array<Habit>;
+  @Prop() private user!: User;
+
+  // Activity Store
+  @Getter("activities", { namespace: "activity" }) activities:
+    | Activities
+    | undefined;
+  @Getter("heatmapHabitActivities", { namespace: "activity" })
+  heatmapHabitActivities: Array<Activity> | undefined;
+  @Action("fetchActivities", { namespace: "activity" }) fetchActivities: any;
+  @Action("persistActivity", { namespace: "activity" }) persistActivity: any;
+  @Action("deleteActivities", { namespace: "activity" }) deleteActivities: any;
+
+  mounted() {
+    this.fetchActivities();
+  }
+
+  activityCreateSubmit(habitId: string, userId: string, amount: number) {
+    this.persistActivity({
+      habitId: habitId,
+      userId: userId,
+      amount: amount,
+    });
+    this.fetchActivities();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.habit {
+  margin: 16px;
+}
+@media (min-width: 768px) {
+  .habit {
+    margin: 64px;
+  }
+}
+
+.activityChart {
+  margin-top: 8px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div v-if="user">
-      <!-- User Header -->
       <UserHeader
         :user="user"
         :habits="habits"
@@ -9,56 +8,33 @@
         @delete="userDeleteSubmit()"
       />
 
-      <!-- Habit List -->
-      <div v-for="habit in habits" :key="habit.name" class="habit">
-        <!-- Habit List Single -->
-        <HabitHeader :habit="habit" :activities="heatmapHabitActivities(habit._id)" />
-
-        <!-- Habit Activity Create -->
-        <ActivityCreate @submit="activityCreateSubmit(habit._id, user._id, $event)" />
-
-        <br />
-
-        <!-- Habit Activity Chart -->
-        <ActivityChart
-          v-if="heatmapHabitActivities(habit._id)"
-          :activities="heatmapHabitActivities(habit._id)"
-        />
-      </div>
+      <HabitList :habits="habits" :user="user" />
     </div>
 
-    <!-- User Create -->
     <UserCreate v-else @submit="userCreateSubmit($event)" />
   </div>
 </template>
 
 <script lang="ts">
-import { State, Action, Getter } from "vuex-class";
+import { Habit } from "@/store/habit/types";
+import { User } from "@/store/user/types";
+import { Activities } from "@/store/activity/types";
 
-import ActivityChart from "@/components/ActivityChart.vue";
-
-import { UserState, User } from "@/store/user/types";
 import UserCreate from "@/components/UserCreate.vue";
 import UserHeader from "@/components/UserHeader.vue";
-
-import { HabitState, Habit } from "@/store/habit/types";
-import HabitHeader from "@/components/HabitHeader.vue";
-
-import { ActivityState, Activity, Activities } from "@/store/activity/types";
-import ActivityCreate from "@/components/ActivityCreate.vue";
+import HabitList from "@/components/HabitList.vue";
 
 import Vue from "vue";
+import { Action, Getter } from "vuex-class";
 import Component from "vue-class-component";
 
 @Component({
   name: "Home",
   components: {
-    ActivityChart,
-    HabitHeader,
     UserCreate,
     UserHeader,
-    ActivityCreate
-  }
+    HabitList,
+  },
 })
 export default class Home extends Vue {
   // User Store
@@ -76,32 +52,18 @@ export default class Home extends Vue {
   @Getter("activities", { namespace: "activity" }) activities:
     | Activities
     | undefined;
-  @Getter("heatmapHabitActivities", { namespace: "activity" })
-  heatmapHabitActivities: Array<Activity> | undefined;
-  @Action("fetchActivities", { namespace: "activity" }) fetchActivities: any;
-  @Action("persistActivity", { namespace: "activity" }) persistActivity: any;
   @Action("deleteActivities", { namespace: "activity" }) deleteActivities: any;
 
   mounted() {
     this.fetchUser();
     this.fetchHabits();
-    this.fetchActivities();
   }
 
   userCreateSubmit(username: string) {
     this.persistUser({
-      username: username
+      username: username,
     });
     this.fetchUser();
-  }
-
-  activityCreateSubmit(habitId: string, userId: string, amount: number) {
-    this.persistActivity({
-      habitId: habitId,
-      userId: userId,
-      amount: amount
-    });
-    this.fetchActivities();
   }
 
   userDeleteSubmit() {
