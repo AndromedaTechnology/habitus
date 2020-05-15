6f8cfe4632c53f81d5c6c13a1c741309f8dee3d4
Mario <marionovak5@gmail.com>
2020-05-15 21:53:40 +0200

    Icons updated

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     pwa: {
         name: 'Habitus',
+        iconPaths: {
+            favicon16: 'img/icons/favicon-16x16.png',
+            favicon32: 'img/icons/favicon-32x32.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon.png',
+            maskIcon: 'img/icons/safari-pinned-tab.svg',
+            msTileImage: 'img/icons/msapplication-icon-144x144.png'
+        }
     }
 }
