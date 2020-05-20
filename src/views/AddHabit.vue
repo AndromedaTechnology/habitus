aa0f2189bbaff2c56fc572d6886a35c725424cf0
Mario <marionovak5@gmail.com>
2020-05-20 23:29:39 +0200

    AddHabit view: margins: container, row, column added

--- src/views/AddHabit.vue
+++ src/views/AddHabit.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <HabitCreate @submit="habitCreateSubmit($event)" />
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="12">
+        <HabitCreate @submit="habitCreateSubmit($event)" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
