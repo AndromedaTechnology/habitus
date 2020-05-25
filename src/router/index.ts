5a2ca31e5487d950c45bfca1d26b498ee923e198
Mario <marionovak5@gmail.com>
2020-05-25 03:21:54 +0200

    Router: scroll behaviour: savedPosition or (0, 0)

--- src/router/index.ts
+++ src/router/index.ts
@@ -36,6 +36,13 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
 });
 
 export default router;
