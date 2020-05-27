93d0744aabb3363bf38418135ed09e67a86680c0
Mario <marionovak5@gmail.com>
2020-05-27 14:30:05 +0200

    Stats: charts next to each other on md size

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,12 +1,10 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" sm="12">
+      <v-col cols="12" sm="12" md="6">
         <OverallChart :habits="habits" :activities="activities" />
       </v-col>
-    </v-row>
-    <v-row class="mt-12">
-      <v-col cols="12" sm="12">
+      <v-col cols="12" sm="12" md="6">
         <DayChart :habits="habits" :activities="activities" />
       </v-col>
     </v-row>
