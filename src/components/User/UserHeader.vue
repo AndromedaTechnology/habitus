0f7d711a6eed0d6076a4fa84581b2dabc6e1ed07
Mario <marionovak5@gmail.com>
2021-05-21 18:33:06 +0200

    UserHeader: clean-up

--- src/components/User/UserHeader.vue
+++ src/components/User/UserHeader.vue
@@ -1,25 +1,22 @@
 <template>
-  <v-container class="userHeader">
+  <v-container class="text-center">
     <v-row>
-      <v-col cols="12" sm="12">
+      <v-col cols="12" sm="12" class="pa-12">
         <!-- Edit -->
         <div v-if="allowEdit">
           <v-btn @click="editDialog = true">Edit</v-btn>
         </div>
-
         <!-- Username -->
         <h1 class="mt-4">
           <span>@</span>
           <span>{{ user.username }}</span>
         </h1>
-
         <!-- Health -->
         <v-chip class="mt-4" :color="getHealth() >= 0 ? '#42b983' : '#b94278'">
           <h4>{{ getHealth() >= 0 ? '+' + getHealth() : getHealth() }} HEALTH</h4>
         </v-chip>
       </v-col>
     </v-row>
-
     <v-dialog
       v-model="editDialog"
       fullscreen
@@ -36,15 +33,11 @@
     </v-dialog>
   </v-container>
 </template>
-
 <script lang="ts">
-import UserEdit from "@/components/User/UserEdit.vue";
-
 import { User } from "@/store/user/types";
-
 import { Action, Getter } from "vuex-class";
+import UserEdit from "@/components/User/UserEdit.vue";
 import { Vue, Component, Prop } from "vue-property-decorator";
-
 @Component({
   components: {
     UserEdit,
@@ -53,27 +46,17 @@ import { Vue, Component, Prop } from "vue-property-decorator";
 export default class UserHeader extends Vue {
   @Prop() private user!: User;
   @Prop({ default: false, type: Boolean }) allowEdit?: boolean;
+  @Action("updateUser", { namespace: "user" }) updateUser: any;
+  @Getter("getHealth", { namespace: "activity" }) getHealth: any;
 
   editDialog = false;
-
-  @Getter("getHealth", { namespace: "activity" })
-  getHealth: any;
-  @Action("updateUser", { namespace: "user" }) updateUser: any;
 
   handleUpdate(data: {}) {
     this.updateUser({ user: this.user, data: data });
   }
-
   handleDelete() {
     this.editDialog = false;
     this.$emit("delete");
   }
 }
 </script>
-
-<style scoped lang="scss">
-.userHeader {
-  text-align: center;
-  padding: 64px;
-}
-</style>
