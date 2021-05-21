f4964769f0235b6c09e1e713702050d7fe857ecf
Mario <marionovak5@gmail.com>
2021-05-21 18:55:22 +0200

    Habit: white-space removed

--- src/views/Habit.vue
+++ src/views/Habit.vue
@@ -1,13 +1,11 @@
 <template>
   <div v-if="user && habit">
     <v-alert text class="pa-12">
-      <!-- Edit -->
+      <!-- Edit Button -->
       <div class="text-center mt-4">
         <v-btn @click="editDialog = true" large>Edit</v-btn>
       </div>
-
       <!-- Header -->
-
       <HabitHeader
         class="mt-4 text-center"
         v-if="habit"
@@ -19,8 +17,7 @@
     <v-container>
       <v-row>
         <v-col cols="12" sm="8" offset-sm="2">
-          <!-- Edit  -->
-
+          <!-- Edit Dialog -->
           <v-dialog
             v-model="editDialog"
             fullscreen
@@ -35,19 +32,15 @@
               @close="editDialog = false"
             />
           </v-dialog>
-
           <!--  Activity Chart -->
-
           <ActivityChart
             v-if="getActivities(habit._id)"
-            class="activityChart"
+            class="mt-4"
             :habit="habit"
             :activities="getActivities(habit._id)"
           />
-
           <!-- Activity List -->
-
-          <div class="activityList">
+          <div class="mt-12">
             <ActivityHeader
               v-for="activity in getActivities(habit._id, true)"
               :key="activity._id"
@@ -60,22 +53,17 @@
     </v-container>
   </div>
 </template>
-
 <script lang="ts">
+import { Datetime } from "vue-datetime";
+import { User } from "@/store/user/types";
+import "vue-datetime/dist/vue-datetime.css";
+import { Action, Getter } from "vuex-class";
+import { Activity } from "@/store/activity/types";
 import HabitEdit from "@/components/Habit/HabitEdit.vue";
 import HabitHeader from "@/components/Habit/HabitHeader.vue";
-import ActivityHeader from "@/components/Activity/ActivityHeader.vue";
-import ActivityChart from "@/components/Activity/ActivityChart.vue";
-
-import { User } from "@/store/user/types";
-import { Activity, Activities } from "@/store/activity/types";
-
-import { Datetime } from "vue-datetime";
-import "vue-datetime/dist/vue-datetime.css";
-
-import { Action, Getter } from "vuex-class";
 import { Component, Vue, Watch } from "vue-property-decorator";
-
+import ActivityChart from "@/components/Activity/ActivityChart.vue";
+import ActivityHeader from "@/components/Activity/ActivityHeader.vue";
 @Component({
   name: "Habit",
   components: {
@@ -88,24 +76,18 @@ import { Component, Vue, Watch } from "vue-property-decorator";
 })
 export default class Habit extends Vue {
   // User
-
   @Getter("user", { namespace: "user" }) user: User | undefined;
   @Action("fetchUser", { namespace: "user" }) fetchUser: any;
-
   // Habit
-
   @Getter("getHabit", { namespace: "habit" }) getHabit!: (
     habitId: string
   ) => Habit | null;
   @Action("updateHabit", { namespace: "habit" }) updateHabit: any;
   @Action("deleteHabit", { namespace: "habit" }) deleteHabit: any;
-
   // Habits
   @Getter("habits", { namespace: "habit" }) habits: Array<Habit> | undefined;
   @Action("fetchHabits", { namespace: "habit" }) fetchHabits: any;
-
   // Activity
-
   @Getter("getActivities", { namespace: "activity" }) getActivities:
     | Array<Activity>
     | undefined;
@@ -113,10 +95,9 @@ export default class Habit extends Vue {
   @Action("deleteHabitActivities", { namespace: "activity" })
   deleteHabitActivities: any;
 
+  editDialog = false;
   habitId: string | null = null;
   habit: Habit | undefined | null = null;
-
-  editDialog = false;
 
   mounted() {
     this.fetchUser();
@@ -129,7 +110,6 @@ export default class Habit extends Vue {
   })
   onRouteChanged(value: any, oldValue: any) {
     if (!value) return;
-
     if (value.params.id) {
       this.habitId = value.params.id;
       this.fetchHabit();
@@ -142,7 +122,6 @@ export default class Habit extends Vue {
   })
   onPropertyChanged(value: any, oldValue: any) {
     if (!value) return;
-
     this.fetchHabit();
   }
 
@@ -158,18 +137,9 @@ export default class Habit extends Vue {
 
   handleDelete() {
     this.editDialog = false;
-
     this.deleteHabitActivities(this.habit);
     this.deleteHabit(this.habit);
-
     this.$router.push({ name: "home" });
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.activityChart,
-.activityList {
-  margin-top: 32px;
-}
-</style>
