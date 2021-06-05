2c7a068dc057a45435f09841b722072649cded79
Mario <marionovak5@gmail.com>
2021-06-05 21:44:53 +0200

    Fix: Api Client: prefix .env var name

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL + process.env.VUE_APP_API_PREFX,
+  baseURL: process.env.VUE_APP_API_URL + process.env.VUE_APP_API_PREFIX,
   headers: {
     "Content-type": "application/json",
   },
